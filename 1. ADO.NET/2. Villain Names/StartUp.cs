5754e2e9ed2bb57a7c9cf408b6c575ef83063159
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-02-16 08:38:26 +0300

    Add files via upload

---
+++ 1. ADO.NET/2. Villain Names/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _2._Villain_Names
+{
+    class StartUp
+    {
+        private const string ConnectionStr = @"Server=LAPTOP-9JDUN69F;Database=MinionsDB; Integrated Security = true;";
+        public static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionStr);
+
+            sqlConnection.Open();
+
+            string sqlGetVillainNamesQueryTest = @"SELECT v.Name, COUNT(mv.VillainId) AS [Number of Minions]
+                                                    FROM Villains AS v
+                                                    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                                    GROUP BY v.Name
+                                                    HAVING COUNT(mv.VillainId) > 3
+                                                    ORDER BY COUNT(mv.VillainId)";
+
+            using SqlCommand sqlGetResultCmd = new SqlCommand(sqlGetVillainNamesQueryTest, sqlConnection);
+
+            using SqlDataReader reader = sqlGetResultCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Name"]?.ToString()} - {reader["Number of Minions"]?.ToString()}");
+            }
+
+        }
+    }
+}
---
+++ 1. ADO.NET/3. Minion Names/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _3._Minion_Names
+{
+    class StartUp
+    {
+        private const string ConnectionString = @"Server=LAPTOP-9JDUN69F;Database=MinionsDB; Integrated Security=true;";
+
+        public static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+            sqlConnection.Open();
+
+            StringBuilder sb = new StringBuilder();
+
+            int villainId = int.Parse(Console.ReadLine());
+
+            string getVillainNameQueryText = @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+            using SqlCommand getVillainNameCmd = new SqlCommand(getVillainNameQueryText, sqlConnection);
+
+            getVillainNameCmd.Parameters.AddWithValue("@villainId", villainId);
+
+            string villainName = getVillainNameCmd.ExecuteScalar()?.ToString();
+
+            if (villainName == null)
+            {
+                sb.AppendLine($"No villain with ID {villainId} exists in the database.");
+            }
+            else
+            {
+                sb.AppendLine($"Villain: {villainName}");
+                string getMinionsInfoQueryText = @"SELECT m.Name, m.Age 
+	                                                FROM MinionsVillains AS mv
+	                                                JOIN Minions AS m ON mv.MinionId = m.Id
+	                                                WHERE VillainId = @villainId
+	                                                ORDER BY m.Name";
+
+                SqlCommand getMinionsNameAndAgeCmd = new SqlCommand(getMinionsInfoQueryText, sqlConnection);
+
+                getMinionsNameAndAgeCmd.Parameters.AddWithValue("@villainId", villainId);
+
+                using SqlDataReader reader = getMinionsNameAndAgeCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    int rowNum = 1;
+                    while (reader.Read())
+                    {
+                        sb.AppendLine($"{rowNum}. {reader["Name"]?.ToString()} {reader["Age"]?.ToString()}");
+                        rowNum++;
+
+                    }
+                }
+                else
+                {
+                    sb.AppendLine("(no minions)");
+                }
+
+                Console.WriteLine(sb.ToString().TrimEnd());
+            }
+        }
+    }
+}
