172ffb83bdd28efa64836900518256ff48c8531a
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-03-08 05:02:18 +0300

    Add files via upload

---
+++ 5. Linq/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,138 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Album>(album =>
+            {
+                album.HasKey(a => a.Id);
+
+                album.Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(40)
+                .IsUnicode();
+
+                album.Property(a => a.ReleaseDate)
+                .IsRequired();
+
+                album.HasOne(a => a.Producer)
+                .WithMany(p => p.Albums)
+                .HasForeignKey(a => a.ProducerId);
+            });
+
+            builder.Entity<Performer>(performer =>
+            {
+                performer.HasKey(p => p.Id);
+
+                performer.Property(p => p.FirstName)
+                .HasMaxLength(20)
+                .IsRequired()
+                .IsUnicode();
+
+                performer.Property(p => p.LastName)
+                .HasMaxLength(20)
+                .IsRequired()
+                .IsUnicode();
+
+                performer.Property(p => p.Age)
+                .IsRequired();
+
+                performer.Property(p => p.NetWorth)
+                .IsRequired();
+            });
+
+            builder.Entity<Producer>(producer =>
+            {
+                producer.HasKey(p => p.Id);
+
+                producer.Property(p => p.Name)
+                .HasMaxLength(30)
+                .IsRequired()
+                .IsUnicode();
+            });
+
+            builder.Entity<Song>(song =>
+            {
+                song.HasKey(s => s.Id);
+
+                song.Property(s => s.Name)
+                .HasMaxLength(20)
+                .IsRequired()
+                .IsUnicode();
+
+                song.Property(s => s.Duration)
+                .IsRequired();
+
+                song.Property(s => s.CreatedOn)
+                .IsRequired();
+
+                song.Property(s => s.Genre)
+                .IsRequired();
+
+                song.Property(s => s.Price)
+                .IsRequired();
+
+                song.HasOne(s => s.Album)
+                .WithMany(a => a.Songs)
+                .HasForeignKey(s => s.AlbumId);
+
+                song.HasOne(s => s.Writer)
+                .WithMany(w => w.Songs)
+                .HasForeignKey(s => s.WriterId);
+            });
+
+            builder.Entity<SongPerformer>(entity =>
+            {
+                entity.HasKey(sp => new { sp.SongId, sp.PerformerId });
+
+                entity.HasOne(sp => sp.Song)
+                .WithMany(s => s.SongPerformers)
+                .HasForeignKey(sp => sp.SongId);
+
+                entity.HasOne(sp => sp.Performer)
+                .WithMany(p => p.PerformerSongs)
+                .HasForeignKey(sp => sp.PerformerId);
+            });
+
+            builder.Entity<Writer>(writer =>
+            {
+                writer.HasKey(w => w.Id);
+
+                writer.Property(w => w.Name)
+                .HasMaxLength(20)
+                .IsRequired()
+                .IsUnicode();
+
+            });
+        }
+    }
+}
---
+++ 5. Linq/MusicHub/StartUp.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using MusicHub.Data;
+using MusicHub.Initializer;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicHub
+{
+        public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(ExportSongsAboveDuration(context, 4));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var albums = context
+                .Albums
+                .ToList()
+                .Where(a => a.ProducerId == producerId)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    Releasedate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs
+                        .Select(s => new
+                        {
+                            SongName = s.Name,
+                            Price = s.Price,
+                            Writer = s.Writer.Name
+                        })
+                        .ToList()
+                        .OrderByDescending(s=>s.SongName)
+                        .ThenBy(s=>s.Writer)
+                        .ToList(),
+                    AlbumPrice = a.Price
+                })
+                .OrderByDescending(a=>a.AlbumPrice)
+                .ToList();
+
+            foreach (var album in albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.Releasedate}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine("-Songs:");
+
+                var index = 1;
+
+                foreach (var song in album.Songs)
+                {
+                    sb.AppendLine($"---#{index}");
+                    sb.AppendLine($"---SongName: {song.SongName}");
+                    sb.AppendLine($"---Price: {song.Price:f2}");
+                    sb.AppendLine($"---Writer: {song.Writer}");
+
+                    index++;
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            StringBuilder sb = new StringBuilder();
+            var songs = context
+                .Songs
+                .ToList()
+                .Where(s => s.Duration.TotalSeconds > duration)
+                .Select(s => new
+                {
+                    SongName = s.Name,
+                    Performer = s.SongPerformers
+                                .Select(sp =>  sp.Performer.FirstName + " " + sp.Performer.LastName)
+                                .FirstOrDefault(),
+                    Writer = s.Writer.Name,
+                    Producer = s.Album.Producer.Name,
+                    Duration = s.Duration
+                })
+                .OrderBy(s => s.SongName)
+                .ThenBy(s => s.Writer)
+                .ThenBy(s => s.Performer)
+                .ToList();
+
+            int counter = 1;
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{counter++}")
+                    .AppendLine($"---SongName: {song.SongName}")
+                    .AppendLine($"---Writer: {song.Writer}")
+                    .AppendLine($"---Performer: {song.Performer}")
+                    .AppendLine($"---AlbumProducer: {song.Producer}")
+                    .AppendLine($"---Duration: {song.Duration:c}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
