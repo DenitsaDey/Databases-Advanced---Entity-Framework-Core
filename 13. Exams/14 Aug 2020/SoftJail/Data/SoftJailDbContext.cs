b8560d80fbcef46939702aca2047007764334505
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2021-03-31 05:55:48 +0300

    Add files via upload

---
+++ 13. Exams/14 Aug 2020/SoftJail/Data/SoftJailDbContext.cs
@@ -0,0 +1,75 @@
+﻿namespace SoftJail.Data
+{
+	using Microsoft.EntityFrameworkCore;
+    using SoftJail.Data.Models;
+
+    public class SoftJailDbContext : DbContext
+	{
+		public SoftJailDbContext()
+		{
+		}
+
+		public SoftJailDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+        public DbSet<Cell> Cells { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Mail> Mails { get; set; }
+        public DbSet<Officer> Officers { get; set; }
+        public DbSet<OfficerPrisoner> OfficersPrisoners { get; set; }
+        public DbSet<Prisoner> Prisoners { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder
+					.UseSqlServer(Configuration.ConnectionString);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<OfficerPrisoner>(entity =>
+			{
+				entity.HasKey(op => new { op.PrisonerId, op.OfficerId });
+
+				entity.HasOne(op => op.Prisoner)
+				.WithMany(op => op.PrisonerOfficers)
+				.HasForeignKey(op => op.PrisonerId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+				entity.HasOne(op => op.Officer)
+				.WithMany(op => op.OfficerPrisoners)
+				.HasForeignKey(op => op.OfficerId)
+				.OnDelete(DeleteBehavior.Restrict);
+			});
+
+			builder.Entity<Mail>(entity =>
+			{
+				entity.HasOne(m => m.Prisoner)
+				.WithMany(p => p.Mails)
+				.HasForeignKey(m => m.PrisonerId)
+				.OnDelete(DeleteBehavior.Restrict);
+			});
+
+			builder.Entity<Cell>(entity =>
+			{
+				entity.HasOne(c => c.Department)
+				.WithMany(d => d.Cells)
+				.HasForeignKey(c => c.DepartmentId)
+				.OnDelete(DeleteBehavior.Restrict);
+			});
+
+			builder.Entity<Prisoner>(entity =>
+			{
+				entity.HasOne(p => p.Cell)
+				.WithMany(c => c.Prisoners)
+				.HasForeignKey(p => p.CellId)
+				.OnDelete(DeleteBehavior.Restrict);
+			});
+		}
+	}
+}
---
+++ 13. Exams/14 Aug 2020/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,65 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Linq;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers
+                                .Select(o => new
+                                {
+                                    OfficerName = o.Officer.FullName,
+                                    Department = o.Officer.Department.Name
+                                })
+                                .OrderBy(o => o.OfficerName)
+                                .ToList(),
+                    TotalOfficerSalary = decimal.Parse(p.PrisonerOfficers.Sum(po => po.Officer.Salary).ToString("f2"))
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToList();
+
+            string json = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+            return json;
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            var names = prisonersNames.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            var result = context.Prisoners
+                .Where(p => names.Contains(p.FullName))
+                .Select(p => new PrisonerViewModel
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = p.Mails.Select(m => new EncryptedMessageViewModel
+                    {
+                        Description = string.Join("", m.Description.Reverse())
+                    })
+                    .ToArray()
+                })
+                .OrderBy(p=>p.Name)
+                .ThenBy(p=>p.Id)
+                .ToList();
+
+            var xml = XmlConverter.Serialize(result, "Prisoners");
+
+            return xml;
+        }
+    }
+}
